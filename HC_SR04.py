48a80a42c820c2fbfdd60b23b879cb639f203b29
dnjs2721 <skadu66@gmail.com>
2022-03-19 14:20:21 +0900

    2022_03_19

---
+++ HC_SR04.py
@@ -0,0 +1,44 @@
+import RPi.GPIO as GPIO
+from email import header
+import requests, json
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(18, GPIO.IN)
+
+def distance():
+    GPIO.output(17, False)
+    time.sleep(0.5)
+
+    GPIO.output(17, True)
+    time.sleep(0.00001)
+    GPIO.output(17, False)
+   
+    while GPIO.input(18) == 0:
+        start = time.time()
+       
+    while GPIO.input(18) == 1:
+        stop = time.time()
+       
+    time_interval = stop - start
+    distance = time_interval * 17000
+    distance = round(distance, 2)
+    return distance
+
+def distance_result():
+    distance_result = distance()
+    data = json.dumps({
+            'helmetId' : 'H0001',
+            'distance' : distance_result
+            })
+    header = {'Content-type' : 'application/json'}
+    res = requests.post('http://125.177.137.35:8080/api/shocksensor', data=data, headers=header)
+    return distance_result
+
+
+if __name__ == '__main__':
+
+    while True:
+        print("Distance => ", distance(), "cm")
--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import shock_detection
-import distance
-import RPi.GPIO as GPIO
+# import distance
+import HC_SR04
 import time
 
-GPIO.setmode(GPIO.BCM)
 time.sleep(1)
 
 
 while True:
-    wearing = distance.distance()
+    distance = HC_SR04.distance()
     shock = shock_detection.shock()
 
-    distance.distance_result()
-    if wearing == 0:
+    HC_SR04.distance_result()
+    if distance < 6:
         if shock == 1:
             shock_detection.shock_result()
-            print("충격이 감지")
+            print("--------------------------")
+            print("착용 중")
+            print("Distance => ", distance, "cm")
+            print("충격 감지")
         else:
             shock_detection.shock_result()
+            print("--------------------------")
+            print("착용 중")
+            print("Distance => ", distance, "cm")
             print("충격이 감지되지 않았음")
     else:
+        print("--------------------------")
+        print("착용 아님")
         print("착용상태 아님")
 
     time.sleep(1)
